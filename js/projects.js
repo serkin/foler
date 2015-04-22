36850a6fc8c75baf2d407bb1886a5d3c2a608597
Serkin Alexander <serkin.alexander@gmail.com>
2015-04-22 22:18:13 +0300

    Fix project_block id

--- js/projects.js
+++ js/projects.js
@@ -8,15 +8,22 @@ var projects = {
 
             for (var i in response.response) {
                 out += response.response[i].name + "<br />";
-
-
-                
             }
 
-            var el = document.querySelectorAll('#projects');
+            var el = document.querySelectorAll('#projects_block');
                 el[0].innerHTML = out;
 
             console.log(out);
         });
+    },
+
+    selectProjectById: function(idProject) {},
+    export: function(idProject, type) {},
+    
+    
+    ProjectForm: {
+        save: function(){},
+        clear: function() {},
+        fillWithProjectData: function(idProject) {}
     }
 };
