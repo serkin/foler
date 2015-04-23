1fe3e9cc8788bed517c05e33cf4bea93a053ffd8
Serkin Alexander <serkin.alexander@gmail.com>
2015-04-23 17:24:53 +0300

    project\save implementation

--- controllers/project/save.php
+++ controllers/project/save.php
@@ -2,9 +2,29 @@
 
 
 
-$app['controllers']['project/getall'] = function ($app, $request){
+$app['controllers']['project/save'] = function ($app, $request){
     
-    $projects = $app['foler']->getAllProjects();
-    Response::responseWithSuccess($projects);
+    
+    // Checks if id_oriject exists
+    
+    
+    // Save project
+    
+    // Edit project
+    $data = (array)json_decode($request['form']);
+    
+    ChromePhp::log($data);
+    $idProject = !empty($data['id_project']) ? $data['id_project'] : null;
+
+    
+    
+    $result = $app['foler']->savePropject($data, $idProject);
+    
+    
+    if($result):
+        Response::responseWithSuccess(['response' => $data, 'message' => 'Project saved']);
+    else:
+        Response::responseWithError($app['foler']->getError()[2]);
+    endif;
     
 };
