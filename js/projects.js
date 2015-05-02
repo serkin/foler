5bf16ebe1dc3544b67a8e3f9310fbd18e2f40ad0
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-02 10:19:52 +0300

    Rewrite projects.reload() with Mustache

--- js/projects.js
+++ js/projects.js
@@ -9,25 +9,11 @@ var projects = {
     },
     reload: function() {
         sendRequest('project/getall',{}, function(response){
+             
+            var template = $('#projectsTemplate').html();
+            var rendered = Mustache.render(template, response.response);
 
-            var out = '<table class="table table-condensed"><thead><th>#</th><th>Name</th><th>Manage</th></thead><tbody>';
-
-            for (var i in response.response.data) {
-                var id = response.response.data[i].id_project;
-                out += "<tr class='project_block' id='project_block_"
-                        + id
-                        + "' OnClick='projects.selectProjectById(" + id + ")'><td>" + id + "</td><td>"
-                        + response.response.data[i].name
-                        + "</td><td><button class='btn btn-danger btn-xs' OnClick='projects.deleteProject("
-                        + id
-                        + ")'>delete</button></td></tr>";
-            }
-            out += '</tbody></table>';
-
-            var el = document.querySelectorAll('#projects_block');
-                el[0].innerHTML = out;
-
-            console.log(out);
+            $('#projects_block').html(rendered);
         });
     },
 
