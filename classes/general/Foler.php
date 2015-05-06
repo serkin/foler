036c290b991ff25fe81ba0b3c831f3952bd29951
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-06 18:57:34 +0300

    Implementation for methods: getAllCodes, saveTranslation, getLanguagesFromProject, getTranslation

--- classes/general/Foler.php
+++ classes/general/Foler.php
@@ -116,16 +116,58 @@ class Foler {
      * @param string $keyword
      * @return array
      */
-    public function getAllCodes($idProject, $keyword = null){}
+    public function getAllCodes($idProject, $keyword = null)
+    {
+
+        $sth = $this->dbh->prepare('SELECT * FROM `code` WHERE `id_project` = ? and `code` like ?');
+
+        $keyword = !is_null($keyword) ? "%$keyword%" : '';
+
+        $sth->bindParam(1, $idProject, PDO::PARAM_INT);
+        $sth->bindParam(2, $keyword, PDO::PARAM_STR);
+        $sth->execute();
+
+        return $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
 
     /**
      * Gets translation according with given code
      * 
-     * @param integer $idProject
-     * @param string $code
+     * @param integer $idCode
      * @return array
      */
-    public function getTranslation($idProject, $code){}
+    public function getTranslation($idCode){
+
+        $idProject = $this->getProjectIdByCodeId($idCode);
+        
+        $languages = $this->getLanguagesFromProject($idProject);
+
+        ChromePhp::log($languages);
+        $sth = $this->dbh->prepare('SELECT * FROM `translation` WHERE `id_code` = ?');
+        $sth->bindParam(1, $idCode, PDO::PARAM_INT);
+
+        $sth->execute();
+
+        foreach($sth->fetchAll(PDO::FETCH_ASSOC) as $record):
+            $result[$record['language']] = $record['translation'];
+        endforeach;
+        
+        ChromePhp::log($result);
+
+        $returnValue = [];
+        $returnValue['id_code'] = $idCode;
+
+        foreach ($languages as $lang):
+            $returnValue['translations'][] = [
+                'language'      => $lang,
+                'translation'   => !empty($result[$lang]) ? $result[$lang] : ''
+            ];
+        endforeach;
+
+        return $returnValue;      
+
+
+    }
 
     /**
      * Get all information about project
@@ -133,16 +175,43 @@ class Foler {
      * @param integer $idProject
      * @return array
      */
-    public function getProjectData($idProject){}
+    public function getProjectById($idProject)
+    {
+        $sth = $this->dbh->prepare("SELECT * FROM `project` WHERE `id_project` = ?");
+        $sth->bindParam(1, $idProject, PDO::PARAM_INT);
+        $sth->execute();
+        return $sth->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    public function getProjectIdByCodeId($idCode)
+    {
+        $sth = $this->dbh->prepare("SELECT `id_project` FROM `code` WHERE `id_code` = ?");
+        $sth->bindParam(1, $idCode, PDO::PARAM_INT);
+        $sth->execute();
+        $arr = $sth->fetch(PDO::FETCH_ASSOC);
+        
+        return $arr['id_project'];
+        
+    }
 
     /**
      * Saves translation code
      * 
-     * @param integer $idProject
      * @param string $code
+     * @param int $idProject
      * @return boolean
      */
-    public function saveCode($idProject, $code){}
+    public function saveCode($code, $idProject)
+    {
+
+        $sth = $this->dbh->prepare('INSERT INTO `code` (`code`,`id_project`) VALUES(?, ?)');
+
+        $sth->bindParam(1, $code, PDO::PARAM_STR);
+        $sth->bindParam(2, $idProject, PDO::PARAM_INT);
+        $sth->execute();
+
+        return $this->dbh->lastInsertId() ? $this->dbh->lastInsertId() : false;
+    }
 
     /**
      * Saves project
@@ -153,7 +222,7 @@ class Foler {
      * @param integer $idProject
      * @return int|boolean False on error
      */
-    public function savePropject($arr, $idProject = null){
+    public function saveProject($arr, $idProject = null){
 
         $returnValue = false;
 
@@ -163,8 +232,10 @@ class Foler {
             $returnValue = $this->dbh->lastInsertId() ? $this->dbh->lastInsertId() : false;
 
         else:
-            $sql = "UPDATE `project` SET `name` = '{$arr['name']}', `path` = '{$arr['path']}', `languages` = '{$arr['languages']}' WHERE `id` = " . $idProject;
-            $returnValue = $this->dbh->exec($sql) ? true : false;
+            
+            // TODO bind params
+            $sql = "UPDATE `project` SET `name` = '{$arr['name']}', `path` = '{$arr['path']}', `languages` = '{$arr['languages']}' WHERE `id_project` = " . $idProject;
+            $returnValue = $this->dbh->exec($sql) ? $idProject : false;
         endif;
 
         return $returnValue;
@@ -186,20 +257,42 @@ class Foler {
 
     /**
      * 
-     * @param integer $idProject
-     * @param string $code
      * @param array $arr
+     * @param integer $idCode
      * @return boolean
      */
-    public function saveTranslation($idProject, $code, $arr){}
+    public function saveTranslation($arr, $idCode)
+    {
+        
+
+        $languages = $this->getLanguagesFromProject($this->getProjectIdByCodeId($idCode));
+
+        foreach ($languages as $language):
+            
+            if(isset($arr[$language])):
+                $value = !empty($arr[$language]) ? $arr[$language] : '';
+            
+                $sth = $this->dbh->prepare('INSERT INTO `translation` (`id_code`,`language`, `translation`) VALUES(?, ?, ?)');
+                
+                $sth->bindParam(1, $idCode, PDO::PARAM_INT);
+                $sth->bindParam(2, $language, PDO::PARAM_STR);
+                $sth->bindParam(3, $value, PDO::PARAM_STR);
+
+                if($sth->execute() === false):
+                    return false;
+                endif;
+            endif;
+        endforeach;
+
+        return true;
+    }
     
     /**
      * 
-     * @param integer $idProject
-     * @param string $code
+     * @param integer $idCode
      * @return boolean
      */
-    public function deleteCodeFromProject($idProject, $code){}
+    public function deleteCodeFromProject($idCode){}
     
     /**
      * 
@@ -208,5 +301,17 @@ class Foler {
      * @return boolean
      */
     public function exportProject($idProject, $exportType){}
+    
+    private function getLanguagesFromProject($idProject){
 
+        $returnValue = [];
+
+        $data = $this->getProjectById($idProject);
+        
+        if(!empty($data['languages'])):
+            $returnValue = explode(",", $data['languages']);
+        endif;        
+
+        return $returnValue;
+    }
 }
