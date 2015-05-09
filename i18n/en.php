f7f89c1a2fda23927059cec1b168db26847c1371
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-09 21:31:26 +0300

    Add correct i18n for layout

--- i18n/en.php
+++ i18n/en.php
@@ -1,16 +1,22 @@
 <?php
 
 
-$app['i18n']['en'] = array(
-    'template' => array(
-        'code_placeholder' => 'code',
-        'button_save' => 'Save',
-        '' => '',
-        '' => '',
-        '' => '',
-        '' => '',
-        '' => '',
-        '' => '',
-        '' => ''
-    )
-);
+$app['i18n']['en'] = [
+    'layout' => [
+        'code_placeholder'  => 'code',
+        'save'              => 'Save',
+        'code'              => 'Code',
+        'title'             => 'Foler - single page translation system',
+        'name'              => 'Name',
+        'manage'            => 'Manage',
+        'languages'         => 'Languages',
+        'path'              => 'Path',
+        'clear'             => 'Clear',
+        'new_translation'   => 'Add new translation',
+        'add_project'       => 'Add/edit project',
+        'delete'            => 'delete'
+        ],
+    'foler' => [
+        'project_saved' => 'Project saved'
+    ]
+];
