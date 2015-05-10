ee23960447eb67664ca8f0664ca6eeee88714b82
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-10 20:48:28 +0300

    Improve i18n.en

--- i18n/en.php
+++ i18n/en.php
@@ -10,13 +10,24 @@ $app['i18n']['en'] = [
         'name'              => 'Name',
         'manage'            => 'Manage',
         'languages'         => 'Languages',
-        'path'              => 'Path',
+        'path'              => 'Path for export',
         'clear'             => 'Clear',
         'new_translation'   => 'Add new translation',
         'add_project'       => 'Add/edit project',
         'delete'            => 'delete'
         ],
     'foler' => [
-        'project_saved' => 'Project saved'
+        'project_saved' => 'Project saved!',
+        'project_removed' => 'Project removed!',
+        'translation_saved' => 'Translation saved!',
+        'code_removed' => 'Code removed!',
+    ],
+    'errors' => [
+        'empty_code' => 'Code not specified',
+        'empty_id_project' => 'ID project not specified',
+        'empty_project_name' => 'Project name not specified',
+        'empty_project_export_path' => 'Project export path not specified',
+        'not_valid_project_languages' => 'Languages field should be unique two letters string separated by comma',
+        'not_valid_project_code' => 'Code field should consists of only dots, numbers and letters in loser case'
     ]
 ];
