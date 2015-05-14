18b97f6959891e72d45146ac6e6df25a7564391f
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-14 15:41:57 +0300

    Add i18n to js

---
+++ js/i18n.js
@@ -0,0 +1,12 @@
+var locale = 'en';
+var i18n = {
+    en: {
+        no_projects: 'No projects yet. Start with adding new project in the form below',
+        no_codes: 'No codes found.',
+    }
+};
+
+i18n = i18n[locale];
+
+
+ 
