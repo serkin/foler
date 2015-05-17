7ad8d9c6762503f2af8521387b682b119161d472
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-17 19:31:23 +0300

    Add debug to $app config

--- config/header.php
+++ config/header.php
@@ -1,17 +1,44 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $app = array();
 
 $app['config'] = array(
     'db' => array(
         'dsn'      => 'mysql:dbname=foler;host=localhost',
         'user'      => 'root',
-        'password'  => 'CFGHNBV'
-    )
+        'password'  => ''
+    ),
+    'debug' => false
 );
+
+
+
+if($app['config']['debug']):
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+endif;
+
+
 $app['locale'] = 'en';
 
-include 'ChromePhp.php';
+
+/**
+ * Helps us split codes to associative array
+ * 
+ * @param type $string
+ * @param type $value
+ * @param array $arr
+ */
+function joinStringToArr($string, $value, &$arr = array()) {
+    
+        $keys = explode('.', $string);
+
+        $ref = &$arr;
+
+        while ($key = array_shift($keys)) {
+            $ref = &$ref[$key];
+        }
+
+        $ref = $value;
+
+    }
