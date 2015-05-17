859fd9e22e2f023bcf70e8d5dbe5ed52c16620f8
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-17 19:32:20 +0300

    Relink $app['i18n'][$app['locale']]

--- config/footer.php
+++ config/footer.php
@@ -9,10 +9,9 @@ try {
     Response::responseWithError($exc->getMessage());
 }
 
-$i18n = $app['i18n'][$app['locale']];
+$i18n = $app['i18n'] = $app['i18n'][$app['locale']];
 
 
 if(!empty($_REQUEST['action']) && isset($app['controllers'][$_REQUEST['action']])):
     $app['controllers'][$_REQUEST['action']]($app, $_REQUEST);
 endif;
-
