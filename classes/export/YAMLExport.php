827ec0923fba4eeb7616e0e8860154d238dff604
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-17 19:29:12 +0300

    Add export classes

---
+++ classes/export/YAMLExport.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class YAMLExport implements ExportInterface {
+
+    public function export($arr, $directory, $language)
+    {
+
+        $file = $directory . DIRECTORY_SEPARATOR . $language . '.yml';
+        
+        
+
+        $fp = fopen($file, 'w');
+        fwrite($fp, spyc_dump($arr));
+        fclose($fp);
+        
+        return file_exists($file);
+
+    }
+}
