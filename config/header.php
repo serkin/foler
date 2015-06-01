11784978f56fbbb7941aaf9801106a2e4862429c
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-01 13:46:55 +0300

    Code formating

--- config/header.php
+++ config/header.php
@@ -1,16 +1,16 @@
 <?php
 
-$app = array();
+$app = [];
 
-$app['config'] = array(
-    'db' => array(
-        'dsn'      => 'mysql:dbname=foler;host=localhost',
+$app['config'] = [
+    'db' => [
+        'dsn'       => 'mysql:dbname=foler;host=localhost',
         'user'      => 'foler',
         'password'  => ''
-    ),
+    ],
     'url' => $_SERVER['PHP_SELF'],
     'debug' => false
-);
+];
 
 
 
@@ -32,14 +32,14 @@ $app['locale'] = 'en';
  */
 function joinStringToArr($string, $value, &$arr = array()) {
     
-        $keys = explode('.', $string);
+    $keys = explode('.', $string);
 
-        $ref = &$arr;
+    $ref = &$arr;
 
-        while ($key = array_shift($keys)) {
-            $ref = &$ref[$key];
-        }
-
-        $ref = $value;
-
+    while ($key = array_shift($keys)) {
+        $ref = &$ref[$key];
     }
+
+    $ref = $value;
+
+}
