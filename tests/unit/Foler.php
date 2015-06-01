f5a9b3ade4d07f2f0ec41e5ab60d9feb445bcb9d
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-01 13:47:29 +0300

    Add first test

--- tests/unit/Foler.php
+++ tests/unit/Foler.php
@@ -1,14 +1,22 @@
 <?php
 
 
-class Foler extends PHPUnit_Framework_TestCase
+class FolerTest extends PHPUnit_Framework_TestCase
 {
+    
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
     public function testDBConnection()
     {
 
-     
+        require dirname(dirname(__DIR__)) . '/classes/general/Foler.php';
+        
+        $foler = new Foler($GLOBALS['db_dsn'], $GLOBALS['db_user'], $GLOBALS['db_password']);
+        $foler->saveProject($arr);
+        $this->assertTrue(true);
 
-       // $this->assertInstanceOf('MongoDB', $dbh, 'Connection not estableshed');
     }
-
 }
