95e7f54c8711caea0eb79c8d432d0f3622efb1f2
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-01 14:30:08 +0300

    Improve testInsertingProjec

--- tests/unit/Foler.php
+++ tests/unit/Foler.php
@@ -9,14 +9,29 @@ class FolerTest extends PHPUnit_Framework_TestCase
         parent::setUp();
     }
 
-    public function testDBConnection()
+    public function testInsertingProject()
     {
 
         require dirname(dirname(__DIR__)) . '/classes/general/Foler.php';
         
+        $arr = [
+            'name'      => 'project 1',
+            'path'      => 'path',
+            'languages' => 'ru,en'
+        ];
+        
         $foler = new Foler($GLOBALS['db_dsn'], $GLOBALS['db_user'], $GLOBALS['db_password']);
-        $foler->saveProject($arr);
-        $this->assertTrue(true);
+        $foler->connect();
+        $id = $foler->saveProject($arr['name'], $arr['path'], $arr['languages']);
+        $project = $foler->getProjectById($id);
+
+        $this->assertTrue(is_numeric($id));
+        $this->assertEquals($project['name'], $arr['name']);
+        $this->assertEquals($project['path'], $arr['path']);
+        $this->assertEquals($project['languages'], $arr['languages']);
+        
+        
+        // Test update project
 
     }
 }
