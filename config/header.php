3328fe07bdc7b7d66a5b8ecde7900ce481f65687
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-02 23:10:51 +0300

    Refactor global function

--- config/header.php
+++ config/header.php
@@ -16,30 +16,8 @@ $app['config'] = [
 
 if($app['config']['debug']):
     error_reporting(E_ALL);
-    ini_set('display_errors', 1);
 endif;
 
 
 $app['locale'] = 'en';
 
-
-/**
- * Helps us split codes to associative array
- * 
- * @param type $string
- * @param type $value
- * @param array $arr
- */
-function joinStringToArr($string, $value, &$arr = []) {
-    
-    $keys = explode('.', $string);
-
-    $ref = &$arr;
-
-    while ($key = array_shift($keys)) {
-        $ref = &$ref[$key];
-    }
-
-    $ref = $value;
-
-}
