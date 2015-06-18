862dd99afce1ea57f2a7726a0c5304e9fe09c272
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-18 14:34:04 +0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,31 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 10, 2015 at 04:33 PM
--- Server version: 5.5.9-log
--- PHP Version: 5.6.0RC4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `foler`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `project`
 --
 
 CREATE TABLE IF NOT EXISTS `project` (
-`id_project` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `languages` varchar(30) NOT NULL,
-  `path` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+    `id_project` int(11) NOT NULL,
+    `name` varchar(30) NOT NULL,
+    `languages` varchar(30) NOT NULL,
+    `path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -34,10 +17,10 @@ CREATE TABLE IF NOT EXISTS `project` (
 --
 
 CREATE TABLE IF NOT EXISTS `translation` (
-  `id_project` int(11) NOT NULL,
-  `code` varchar(50) NOT NULL,
-  `language` varchar(2) DEFAULT NULL,
-  `translation` text
+    `id_project` int(11) NOT NULL,
+    `code` varchar(50) NOT NULL,
+    `language` varchar(2) DEFAULT NULL,
+    `translation` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -48,13 +31,15 @@ CREATE TABLE IF NOT EXISTS `translation` (
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
- ADD PRIMARY KEY (`id_project`);
+ADD PRIMARY KEY (`id_project`),
+ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `translation`
 --
 ALTER TABLE `translation`
- ADD UNIQUE KEY `unique_index` (`id_project`,`code`,`language`), ADD KEY `id_project` (`id_project`);
+ADD UNIQUE KEY `unique_index` (`id_project`,`code`,`language`),
+ADD KEY `id_project` (`id_project`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -64,7 +49,7 @@ ALTER TABLE `translation`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
